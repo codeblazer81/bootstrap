dab4ac4a04de92b00be9a693a8a606f02b4f86f2
Sonu Sud <sonu.sud@gmail.com>
2022-09-28 22:27:04 -0400

    Add APIInfo method to swagger config

--- src/main/java/com/jnd/udacity/restdemo/dogrestapi/config/SwaggerConfig.java
+++ src/main/java/com/jnd/udacity/restdemo/dogrestapi/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.jnd.udacity.restdemo.dogrestapi.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 
 
@@ -8,6 +10,8 @@ import com.google.common.base.Predicate;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -25,4 +29,13 @@ public class SwaggerConfig {
             .build();
     }
     
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Dog REST API",
+                "This API returns a list of dogs.",
+                "1.0",
+                "http://www.udacity.com/tos",
+                new Contact("Udacious Student", "www.udacity.com", "myeaddress@udacity.com"),
+                "License of API", "http://www.udacity.com/license", Collections.emptyList());
+    }
 }
